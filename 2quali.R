8418a70da5440eae6d97735b1291a8e623e1d41d
Hugo <hfournier001@ensc.fr>
2019-04-26 09:48:15 +0200

    scripts commentés
    enfin au début quoi

--- 2quali.R
+++ 2quali.R
@@ -1,4 +1,12 @@
+#######################################
+# Analyse de 2 variables qualitatives #
+#     --> Sexe et Crawling_group      #
+#######################################
+
+# Initialisation
 library(haven)
 dataSet <- read_sav("Kubicek et al._JECP_DataSet.sav")
 dataSet <- subset(dataSet, Age != "NA") #suppression des lignes vides à la fin
 View(dataSet)
+
+#Analyse
--- 2quanti.R
+++ 2quanti.R
@@ -1,4 +1,12 @@
+#######################################
+# Analyse de 2 variables quantitatives #
+#     --> Age et Crawling_Weeks        #
+#######################################
+
+# Initialisation
 library(haven)
 dataSet <- read_sav("Kubicek et al._JECP_DataSet.sav")
 dataSet <- subset(dataSet, Age != "NA") #suppression des lignes vides à la fin
 View(dataSet)
+
+#Analyse
