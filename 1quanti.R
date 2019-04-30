73aa9b46092484d7300576140c20bd42b9be5135
Marie Morelle <mmorelle@ensc.fr>
2019-04-30 15:39:49 +0200

    Ajout de l'analyse avec seulement les enfants qui savent ramper

--- 1quanti.R
+++ 1quanti.R
@@ -7,14 +7,19 @@
 library(haven)
 rm(list=ls())
 dataSet <- read_sav("Kubicek et al._JECP_DataSet.sav")
-dataSet <- subset(dataSet, Age != "NA") #suppression des lignes vides à la fin
+dataSet <- subset(dataSet, Age != "NA") #suppression des lignes vides ? la fin
 View(dataSet)
 
 crawling_weeks <- as.numeric(unlist(dataSet["Crawling_Weeks"]))
 
-# Analyse
+# Analyse avec tout le dataset
 summary(crawling_weeks) #général
 sd(crawling_weeks) #dispersion
 par(mfrow=c(1,2))
-hist(crawling_weeks)
-boxplot(crawling_weeks)
+hist(crawling_weeks, main="Histogramme : répartition de Crawling_Weeks")
+boxplot(crawling_weeks, main="Boxplot : répartition de Crawling_Weeks")
+
+#Analyse avec seulement les enfants qui savent ramper
+#datasetRampants <- dataSet
+datasetRampants <- subset(dataSet, Crawling_Group == 1)
+View(datasetRampants)
